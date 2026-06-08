ed2337b09292e42e929fd9198c52c49b9ce4eea0
Edson <edson_vs09@hotmail.com>
2026-06-08 14:33:30 -0300

    model user e teste

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const connectDB = require("./src/config/db");
 
+const User = require("./src/models/User");
+
 const express = require('express')
 const cors = require('cors');
 
@@ -38,6 +40,28 @@ app.use(express.json());
 app.get("/", (req, res) => {
     res.send("API rodando!");
 });
+
+
+app.get("/test-user", async (req, res) => {
+  try {
+    const user = await User.create({
+      name: "Administrador",
+      email: "admin@escola.edu.br",
+      password: "123456",
+      role: "Administrador"
+    });
+
+    res.json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
